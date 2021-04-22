8b7c4557633958b9a647bd8d7d7a5c813976198f
Andrey Ryshkevich <paulbegitson@mail.ru>
2021-04-22 21:39:59 +0300

    Rocket engine audio added

--- projectboost/Assets/Scripts/RocketMovement.cs
+++ projectboost/Assets/Scripts/RocketMovement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class RocketMovement : MonoBehaviour
 {
     Rigidbody rb;
+    AudioSource audioSource;
     [SerializeField] float speed = 2f;
     [SerializeField] float rotateSpeed = 30f;
     // Start is called before the first frame update
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,16 @@ public class RocketMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * speed * Time.deltaTime, ForceMode.Impulse);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+
+        else
+        {
+            audioSource.Stop();
         }
     }
 
@@ -32,12 +44,16 @@ public class RocketMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
+            rb.freezeRotation = true;
             transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+            rb.freezeRotation = false;
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
+            rb.freezeRotation = true;
             transform.Rotate(Vector3.forward * -rotateSpeed * Time.deltaTime);
+            rb.freezeRotation = false;
         }
     }
 }
