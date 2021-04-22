452e5051f80bd7bec361acb318d534cb95210dc4
Andrey Ryshkevich <paulbegitson@mail.ru>
2021-04-22 23:32:19 +0300

    Scene Reload added

---
+++ projectboost/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly obj");
+                break;
+            case "Finish":
+                Debug.Log("You hit the finish");
+                break;
+            default:
+                ReloadScene();
+                Debug.Log("You're dead");
+                break;
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
