4c52255dca98759316fb0f8135b0f85d45ace33d
Andrey Ryshkevich <paulbegitson@mail.ru>
2021-04-23 19:04:06 +0300

    Application.Quit added

--- projectboost/Assets/Scripts/ObstacleMover.cs
+++ projectboost/Assets/Scripts/ObstacleMover.cs
@@ -17,6 +17,10 @@ public class ObstacleMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
         float cycles = Time.time / period;
 
         const float tau = Mathf.PI * 2;
